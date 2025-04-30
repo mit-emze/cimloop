f38807ba6a1f2d7ba513a9d3b37c471e97592ac6
jricramc <95874593+jricramc@users.noreply.github.com>
2025-04-30 17:46:30 -0400

    example2

---
+++ workspace/models/arch/1_macro/example_traditional2/_tests.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+# fmt: off
+THIS_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+MACRO_NAME = os.path.basename(THIS_SCRIPT_DIR)
+sys.path.append(os.path.abspath(os.path.join(THIS_SCRIPT_DIR, '..', '..', '..', '..')))
+from scripts import utils as utl
+import scripts
+# fmt: on
+
+def test_area_energy_breakdown():
+    """
+    ### Area and energy breakdown
+    This example architecture doesn't have a suite of tests, but you may
+    look at the other example architectures for inspiration.        
+    """
+    results = utl.single_test(utl.quick_run(macro=MACRO_NAME))
+
+    results = utl.parallel_test(
+        utl.delayed(utl.quick_run)(
+            macro=MACRO_NAME,
+            variables=dict(
+                QUANTIZE=q,
+            ),
+            dnn="nestquant_workload",
+        )
+        for q in [False, True]
+    )
+    results.clear_zero_areas()
+    results.clear_zero_energies()
+    return results
+
+if __name__ == "__main__":
+    test_area_energy_breakdown()
---
+++ workspace/models/arch/1_macro/example_traditional2/helper_functions.py
@@ -0,0 +1,2 @@
+def helper_funcion(x: int,  y: int = 0) -> int:
+    return x ** 2 + y
