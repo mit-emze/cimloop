f9064ff81f2be51ae586465ecde55d8f91602a7c
tanner-andrulis <andrulis@mit.edu>
2025-12-08 09:42:05 -0500

    Accuracy DSE Experiment

---
+++ workspace/models/arch/1_macro/accuracy_dse/_tests.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+# fmt: off
+THIS_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+MACRO_NAME = os.path.basename(THIS_SCRIPT_DIR)
+sys.path.append(os.path.abspath(os.path.join(THIS_SCRIPT_DIR, '..', '..', '..', '..')))
+from scripts import utils as utl
+import scripts
+# fmt: on
